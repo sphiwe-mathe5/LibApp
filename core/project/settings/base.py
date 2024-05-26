23623115fe30102be795b520c34d239abc559ea3
Nazire Mathe <nazireemathe@gmail.com>
2024-05-26 08:17:44 +0200

    modified settings.py toto configure statitic files

--- core/project/settings/base.py
+++ core/project/settings/base.py
@@ -3,7 +3,9 @@ from pathlib import Path
 from decouple import config
 
 from pathlib import Path
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = Path(__file__).resolve().parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -69,11 +71,13 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [PROJECT_DIR / "static"]
 
+STATIC_ROOT = BASE_DIR / 'static/'
+
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
