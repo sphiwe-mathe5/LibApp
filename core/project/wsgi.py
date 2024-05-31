1dd40e58ae85c7ed8a2dea7a863360157fa86908
Nazire Mathe <nazireemathe@gmail.com>
2024-05-31 19:54:59 +0200

    some changes

--- core/project/wsgi.py
+++ core/project/wsgi.py
@@ -4,7 +4,7 @@ from django.core.wsgi import get_wsgi_application
 
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.project.settings')
 
 application = get_wsgi_application()
 
