6b4bba056a60e00b085f92bf6cb8d64c815bc8d8
Nazire Mathe <nazireemathe@gmail.com>
2024-06-01 19:05:02 +0200

    email conf

--- core/project/settings.py
+++ core/project/settings.py
@@ -116,7 +116,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_PORT = '456'
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
